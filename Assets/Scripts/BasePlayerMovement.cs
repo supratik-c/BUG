9b1f56ebce6ca844592134e477362dada204d434
James Shore <james@jamesshore.co.uk>
2021-11-02 21:03:14 +0000

    added player gravity and jumping

--- Assets/Scripts/BasePlayerMovement.cs
+++ Assets/Scripts/BasePlayerMovement.cs
@@ -8,8 +8,12 @@ public class BasePlayerMovement : MonoBehaviour
     public CharacterController _characterController;
     public float speed = 12f;
     public float gravity = -9.81f;
-
+    public float JumpHeight = 3f;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
     Vector3 Velocity;
+    bool isGrounded;
     void Start() 
     {
         Debug.Log("Never tell me the odds!");
@@ -19,12 +23,25 @@ public class BasePlayerMovement : MonoBehaviour
     void Update()
     {
 
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (isGrounded && Velocity.y < 0) 
+        {
+            Velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         _characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
 
         Velocity.y += gravity * Time.deltaTime;
 
